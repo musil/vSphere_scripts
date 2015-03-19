6e22b2a02a81d4f8954a0576ff414c5602cc4978
musil <musil@sm-net.cz>
2015-03-19 22:52:46 +0100

    d

---
+++ SSH_start-stop.ps1
@@ -0,0 +1,18 @@
+
+$NEWHost = "ESXi_Server.lab.local"
+
+# start SSH servíce
+Get-VMHost -Name $NEWHost | Foreach
+{
+	Start-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" })
+}
+
+
+# stop SSH function
+function ssh-stop ($NEWHost)
+{
+	Get-VMHost -Name $NEWHost | Foreach
+	{
+		Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" })
+	}
+}
