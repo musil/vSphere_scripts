ad9ad40c3046a2687de0b2bd74fbdaf712da58d8
Stanislav Musil <musil@sm-net.cz>
2024-03-06 20:13:42 +0100

    fix: full Select-Object

--- ESXi/list_ESXi_DNS.ps1
+++ ESXi/list_ESXi_DNS.ps1
@@ -1,4 +1,4 @@
 $esxis=get-vmhost
 foreach ($esxi in $esxis.Name) {
-Get-VMHostNetwork -VMHost $esxi |Select HostName,DomainName,DnsAddress
+Get-VMHostNetwork -VMHost $esxi | Select-Object HostName,DomainName,DnsAddress
 }
